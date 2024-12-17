216a251072eaaf608f14584f6594b14e07a4c65b
toka1300 <caseytokarchuk@gmail.com>
2024-12-17 18:35:21 -0500

    added specific convert property to boolean column, correcteed col. name

--- db/migrate/20241212133812_change_notification_to_boolean_in_alerts.rb
+++ db/migrate/20241212133812_change_notification_to_boolean_in_alerts.rb
@@ -1,5 +1,5 @@
 class ChangeNotificationToBooleanInAlerts < ActiveRecord::Migration[7.2]
   def change
-    change_column :price_alerts, :alert_user, :boolean, using: 'status::boolean'
+    change_column :price_alerts, :alert_user, :boolean, using: 'alert_user::boolean'
   end
 end
