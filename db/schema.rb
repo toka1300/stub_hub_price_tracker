d1aede0bab557fc5e5438846f82557bb9ea834dd
toka1300 <caseytokarchuk@gmail.com>
2024-12-18 10:40:00 -0500

    precompile assets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_12_132533) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_18_152029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -45,6 +45,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_12_132533) do
     t.boolean "email_notification", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   add_foreign_key "price_alerts", "events"
