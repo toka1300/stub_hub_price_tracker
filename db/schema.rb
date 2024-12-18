cc9a1a818b169e7b72b9015d6041f47e5cf13f86
toka1300 <caseytokarchuk@gmail.com>
2024-12-18 13:15:28 -0500

    Make basic user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_12_18_152029) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_18_175831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_12_18_152029) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "price_alerts", "events"
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,65 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = User.new(name: "Casey", email: "casey@casey.com",
+                     password: "password", password_confirmation: "password")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "name shouldn't be too long" do
+    @user.name = "y" * 51
+    assert_not @user.valid?
+  end
+
+  test "email shouldn't be too long" do
+    @user.email = "y" * 256
+    assert_not @user.valid?
+  end
+
+  test "valid emails should be accepted" do
+    valid_addresses = %w[ USER@foo.COM THE_US-ER@foo.bar.org first.last@foo.jp ]
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid?, "#{address.inspect} should be valid email address"
+    end
+  end
+
+  test "invalid emails should not be accepted" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                        foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address} should be invalid"
+    end
+  end
+
+  test "email should be present" do
+    @user.email = "    "
+    assert_not @user.valid?
+  end
+
+  test "email should be unique" do
+    duplicate_user = @user.dup
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "password should not be blank" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should be long enough" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
