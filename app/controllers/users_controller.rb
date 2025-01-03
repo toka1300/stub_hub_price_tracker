a61c1751f4c7e61a8e1f2fa50169d29fdcaa0e53
toka1300 <caseytokarchuk@gmail.com>
2025-01-03 11:58:01 -0500

    allow user edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [ :edit, :update ]
+
+
   def show
     @user = User.find(params[:id])
   end
@@ -40,4 +43,13 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+    # Before filters
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url, status: :see_other
+      end
+    end
 end
--- test/integration/user_edit_test.rb
+++ test/integration/user_edit_test.rb
@@ -6,6 +6,7 @@ class UserEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccesful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template "users/edit"
     patch user_path(@user), params: { user: { name: "",
@@ -15,5 +16,22 @@ class UserEditTest < ActionDispatch::IntegrationTest
     assert_response :unprocessable_entity
     assert_template "users/edit"
     assert_select "div.alert-danger", "The form contains 4 errors."
+  end
+
+  test "successful edit with friendly forwarding" do
+    log_in_as(@user)
+    get edit_user_path(@user)
+    # assert_redirected_to edit_user_url(@user)
+    name = "Casey Tokarchuk"
+    email = "caseytokarchuk@gmail.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    # assert_not flash.empty?
+    # assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
   end
 end
