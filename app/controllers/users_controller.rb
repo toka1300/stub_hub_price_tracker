0e5dc71d65fcb241abc4445bde2b7d7496ecd9ca
toka1300 <caseytokarchuk@gmail.com>
2025-01-04 10:14:12 -0500

    User destroy test done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [ :edit, :update, :index ]
+  before_action :logged_in_user, only: [ :edit, :update, :index, :destroy ]
   before_action :correct_user,   only: [ :edit, :update ]
+  before_action :admin_user,   only: [ :destroy ]
 
   def show
     @user = User.find(params[:id])
@@ -38,6 +39,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id]).destroy
+    flash[:success] = "user successfully deleted"
+    redirect_to users_url, status: :see_other
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -57,5 +64,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url, status: :see_other) unless current_user?(@user)
+  end
+
+  # Confirms admin user
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -55,4 +55,21 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
                                     }
     assert_not @other_user.reload.admin?
   end
+
+  test "logged out user should not be able to delete users" do
+    assert_no_difference "User.count" do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to login_path
+  end
+
+  test "logged in user (not admin) should be redirected to homepage" do
+    log_in_as(@other_user)
+    assert_no_difference "User.count" do
+      delete user_path(@other_user)
+    end
+    assert_response :see_other
+    assert_redirected_to root_path
+  end
 end
