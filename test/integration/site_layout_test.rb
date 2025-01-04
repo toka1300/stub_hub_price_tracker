033fb4acdfaef6022cf49264fefccf9a391904e2
toka1300 <caseytokarchuk@gmail.com>
2025-01-04 08:38:04 -0500

    add users index

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,6 +6,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template "price_alerts/index"
     assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", events_path
+    assert_select "a[href=?]", help_path
     assert_select "a[href=?]", contact_path
+    user = users(:casey)
+    log_in_as user
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(user)
+    assert_select "a[href=?]", edit_user_path(user)
+    assert_select "form[action=?]", logout_path
   end
 end
