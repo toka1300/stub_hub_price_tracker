94e834777d20ec4f538caddbcb0ad08a1382a77b
toka1300 <caseytokarchuk@gmail.com>
2025-01-27 09:36:09 -0500

    destroy price alerts

--- app/controllers/price_alerts_controller.rb
+++ app/controllers/price_alerts_controller.rb
@@ -1,6 +1,7 @@
 class PriceAlertsController < ApplicationController
   include SessionsHelper
   before_action :logged_in_user, only: [ :create, :destroy ]
+  before_action :correct_user, only: :destroy
 
   # def index
   #   @price_alerts = PriceAlert.where(user_id: current_user)
@@ -29,25 +30,29 @@ class PriceAlertsController < ApplicationController
   #   @price_alert = PriceAlert.find(params[:id])
   # end
 
-  def update
-    @price_alert = PriceAlert.find(params[:id])
+  # def update
+  #   @price_alert = PriceAlert.find(params[:id])
 
-    if @price_alert.update(alert_params)
-      redirect_to @price_alert
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
+  #   if @price_alert.update(alert_params)
+  #     redirect_to @price_alert
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
 
   def destroy
-    @price_alert = PriceAlert.find(params[:id])
     @price_alert.destroy
-
+    flash[:success] = "Price alert deleted"
     redirect_to root_path, status: :see_other
   end
 
   private
   def alert_params
     params.require(:price_alert).permit(:event_id, :user_id, :alert_price)
+  end
+
+  def correct_user
+    @price_alert = current_user.price_alerts.find_by(id: params[:id])
+    redirect_to root_url, status: :see_other if @price_alert.nil?
   end
 end
