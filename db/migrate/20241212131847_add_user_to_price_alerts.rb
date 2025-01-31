921e0cf77becac56d9946a1051169cac500a6256
toka1300 <caseytokarchuk@gmail.com>
2025-01-31 09:00:47 -0500

    remove explicit index on user_id

--- db/migrate/20241212131847_add_user_to_price_alerts.rb
+++ db/migrate/20241212131847_add_user_to_price_alerts.rb
@@ -1,6 +1,5 @@
 class AddUserToPriceAlerts < ActiveRecord::Migration[7.2]
   def change
     add_reference :price_alerts, :user, null: false, foreign_key: true
-    add_index :price_alerts, :user_id
   end
 end
