4e8576bd0e13963513272d8cc7eb2ea611ca461c
toka1300 <caseytokarchuk@gmail.com>
2025-01-31 08:58:35 -0500

    fix user to user_id in ref

--- db/migrate/20241212131847_add_user_to_price_alerts.rb
+++ db/migrate/20241212131847_add_user_to_price_alerts.rb
@@ -1,6 +1,6 @@
 class AddUserToPriceAlerts < ActiveRecord::Migration[7.2]
   def change
     add_reference :price_alerts, :user, null: false, foreign_key: true
-    add_index :price_alerts, :user
+    add_index :price_alerts, :user_id
   end
 end
