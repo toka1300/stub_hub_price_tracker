841e98be2726ac2ec440c6324a87cdf357f65df8
toka1300 <caseytokarchuk@gmail.com>
2025-03-01 09:42:34 -0500

    Add currency conversion

--- app/helpers/price_alerts_helper.rb
+++ app/helpers/price_alerts_helper.rb
@@ -1,19 +1,20 @@
+require "net/http"
+require "json"
+
 module PriceAlertsHelper
   def fetch_event_data(url)
-    response = HTTParty.get(url,   headers: {
-      "Accept-Language" => "en-CA",
-      "User-Agent" => "Mozilla/5.0"
-    })
+    response = HTTParty.get(url)
     json_response = response.parsed_response
     parsed_json = json_response.match(/<script id="index-data" type="application\/json">\s*(.*?)\s*<\/script>/)[1]
     json_data = JSON.parse(parsed_json)
     if json_data["errorMessage"] != "Page Not Found"
-      puts json_data
+      currency = json_data["grid"]["formattedMinPrice"][0]
+      exchange_rate = currency == "C" ? 1 : usd_to_cad
       {
         name: json_data["eventName"],
         date: json_data["formattedEventDateTime"],
         venue: json_data["venueName"],
-        live_price_cad: json_data["grid"]["minPrice"].round,
+        live_price_cad: (json_data["grid"]["minPrice"] * exchange_rate).round,
         url: url,
         stubhub_id: json_data["eventId"],
         image_url: json_data["categoryImageUrl"]
@@ -24,3 +25,11 @@ module PriceAlertsHelper
     end
   end
 end
+
+private
+  def usd_to_cad
+    uri = URI("https://api.exchangerate-api.com/v4/latest/usd")
+    response = Net::HTTP.get(uri)
+    json = JSON.parse(response)
+    json["rates"]["CAD"]
+  end
